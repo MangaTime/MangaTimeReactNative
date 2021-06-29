245c2e8524f7527a8263e1f0866df4e50a1725e5
hoangtran-97 <hoangtranxamk@gmail.com>
2021-06-29 17:48:22 +0300

    feat: wrap provider

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Navigator } from './App/Navigator';
-
+import store from './App/redux/store';
 const App = () => {
-  return <Navigator />;
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
 };
 
 export default App;
