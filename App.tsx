9a2d617ea48477c03f330839dbffcfb25fe4b09a
hoangtran-97 <hoangtranxamk@gmail.com>
2021-06-29 18:47:16 +0300

    feat: added persistGate

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Navigator } from './App/Navigator';
-import { store } from './App/redux/store';
+import { persistor, store } from './App/redux/store';
 const App = () => {
   return (
     <Provider store={store}>
-      <Navigator />
+      <PersistGate persistor={persistor}>
+        <Navigator />
+      </PersistGate>
     </Provider>
   );
 };
