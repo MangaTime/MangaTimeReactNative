8a8e05e511719cc3118fbf35591a4187f980bb73
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-04 15:30:16 +0300

    rebase

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [0],
+    'object-curly-newline': [0],
+    'import/extensions': [0],
+    'import/prefer-default-export': [0],
+    'react/jsx-filename-extension': [0],
+    'import/no-unresolved': [0],
+  },
+};
--- App/Views/Home/index.tsx
+++ App/Views/Home/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Button, SafeAreaView, StyleSheet, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Cards } from '../../components/Cards';
 import { decrement, increment } from '../../redux/Counter/counterReducer';
 import { useAppDispatch, useAppSelector } from '../../redux/Hooks';
+
 export const Home = () => {
   const count = useAppSelector((state) => state.persist.counter.value);
   const dispatch = useAppDispatch();
   const onIncrease = () => {
     dispatch(increment());
   };
-
   const onDecrease = () => {
     dispatch(decrement());
   };
@@ -19,8 +18,7 @@ export const Home = () => {
       <Text>{count}</Text>
       <Button onPress={onIncrease} title="+" />
       <Button onPress={onDecrease} title="-" />
-      <Icon size={20} name="glass"></Icon>
-      <Cards />
+      <Icon size={20} name="glass" />
     </SafeAreaView>
   );
 };
