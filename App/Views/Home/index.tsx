a8f0e1acfd7b12077a714c09eb6a1314ea9c084d
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2021-07-05 00:51:14 +0300

    fetched manga detail

--- App/Views/Home/index.tsx
+++ App/Views/Home/index.tsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import { Button, SafeAreaView, StyleSheet, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Cards } from '../../components/Cards';
+// import { Cards } from '../../components/Cards';
 import { MangaList } from '../../Component/MangaList'
-import { decrement, increment } from '../../redux/Counter/counterReducer';
+import { MangaDetail } from '../../Component/MangaDetail'
+// import { decrement, increment } from '../../redux/Counter/counterReducer';
 import { useAppDispatch, useAppSelector } from '../../redux/Hooks';
 export const Home = () => {
-  const count = useAppSelector((state) => state.persist.counter.value);
+  // const count = useAppSelector((state) => state.persist.counter.value);
   const dispatch = useAppDispatch();
-  const onIncrease = () => {
-    dispatch(increment());
-  };
+  // const onIncrease = () => {
+  //   dispatch(increment());
+  // };
 
-  const onDecrease = () => {
-    dispatch(decrement());
-  };
+  // const onDecrease = () => {
+  //   dispatch(decrement());
+  // };
   return (
     <SafeAreaView style={styles.container}>
       <MangaList></MangaList>
-      <Text>{count}</Text>
+      <MangaDetail></MangaDetail>
+      {/* <Text>{count}</Text>
       <Button onPress={onIncrease} title="+" />
       <Button onPress={onDecrease} title="-" />
       <Icon size={20} name="glass"></Icon>
-      <Cards />
+      <Cards /> */}
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
   container: {},
+  mangaList: {width: 100}
 });
--- App/redux/Manga/mangaReducer.ts
+++ App/redux/Manga/mangaReducer.ts
@@ -1,12 +1,23 @@
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
-
-interface Manga {
+import axios from 'axios'
+export interface Chapter {
+    name: string;
+}
+export interface Volume {
+    name: string;
+    chapters?: Chapter[];
+}
+export interface Manga {
+    id: string;
     name: string;
     description: string;
+    cover_art: string;
+    volumes?: Volume[];
 }
 export interface MangaState {
     recentlyUpdatedManga: Manga[];
     recentlyAddedManga: Manga[];
+    mangaDetail?: Manga;
 }
 const initialState: MangaState = {
     recentlyUpdatedManga: [],
@@ -16,8 +27,16 @@ const initialState: MangaState = {
 export const fetchUpdatedManga = createAsyncThunk(
     'manga/fetchUpdatedManga',
     async (userId, thunkAPI) => {
-        const response = fetch("http://api.mangadex.org/manga?limit=3&order[updatedAt]=desc").then(res => res.json())
-        return response
+        const response = await axios.get("http://api.mangadex.org/manga?limit=3&order[updatedAt]=desc&includes[]=cover_art")
+        return response.data
+    }
+)
+
+export const fetchMangaDetail = createAsyncThunk(
+    'manga/fetchMangaDetail',
+    async (manga: Manga, thunkAPI) => {
+        const response = await axios.get(`https://api.mangadex.org/manga/${manga.id}/aggregate`)
+        return response.data
     }
 )
 
@@ -33,15 +52,38 @@ export const mangaSlice = createSlice({
         // Add reducers for additional action types here, and handle loading state as needed
         builder.addCase(fetchUpdatedManga.fulfilled, (state, action) => {
             // Add user to the state array
-            state.recentlyUpdatedManga = (action.payload['results'] as any[]).map(e => {
-                return {
+            console.log(action.payload)
+            state.recentlyUpdatedManga = (action.payload.results as any[]).map(e => {
+                const item = {
+                    id: e.data.id,
                     name: e.data.attributes.title.en, // get only english title and description for now
                     description: e.data.attributes.description.en,
-                    cover_art: (e.relationships as any[]).find(e=>e.type=="cover_art")?.id
+                    cover_art: (e.relationships as any[]).find(e => e.type == "cover_art")?.attributes?.fileName
                 }
+                console.log(`https://uploads.mangadex.org/covers/${item.id}/${item.cover_art}.256.jpg`)
+                return item
             })
             console.log(state.recentlyUpdatedManga)
         })
+
+        builder.addCase(fetchMangaDetail.fulfilled, (state, action) => {
+            // Add user to the state array
+            console.log(action.payload)
+            let mangaDetail = action.meta.arg
+            let volumes = Object.entries(action.payload.volumes).map(([key, value]) => {
+                return {
+                    name: key,
+                    chapters: Object.entries((value as any).chapters).map(([ckey, cvalue]) => {
+                        return {
+                            name: ckey
+                        } as Chapter
+                    })
+                } as Volume
+            })
+            state.mangaDetail = { ...mangaDetail, volumes }
+            console.log(state.mangaDetail)
+        })
+
     },
 });
 
