a6af4a12c3796751ed8eda22b5c86560082c1688
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2021-07-05 00:51:14 +0300

    fetched manga detail

--- App/Component/MangaList/index.tsx
+++ App/Component/MangaList/index.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import { Button, SafeAreaView, StyleSheet, Text } from 'react-native';
+import { FlatList, Button, SafeAreaView, View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import ReduxThunk from 'redux-thunk';
 import { useAppDispatch, useAppSelector } from '../../redux/Hooks';
 
-import {fetchUpdatedManga} from '../../redux/Manga/mangaReducer'
+import { fetchUpdatedManga, fetchMangaDetail, Manga } from '../../redux/Manga/mangaReducer'
 export const MangaList = () => {
   // const count = useAppSelector((state) => state.persist.counter.value);
+  const recentlyUpdatedManga = useAppSelector((state) => state.mangaReducer.recentlyUpdatedManga)
   const dispatch = useAppDispatch();
-  const onIncrease = () => {
+  const updateMangaList = () => {
     dispatch(fetchUpdatedManga());
   };
-
+  const getMangaDetail = (manga:Manga) => {
+    dispatch(fetchMangaDetail(manga))
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <Button onPress={onIncrease} title="Update" />
-      <Text>123456789</Text>
+      <Button onPress={updateMangaList} title="Update" />
+      <FlatList
+        style={styles.mangaList}
+        data={recentlyUpdatedManga}
+        keyExtractor={(item, index) => item.id}
+        renderItem={({ item }) =>
+          <TouchableOpacity  activeOpacity={0.6} onPress={()=>{getMangaDetail(item)}}>
+            <View style={styles.item}>
+              <Image
+                resizeMode="contain"
+                style={styles.thumbnail}
+                source={{
+                  uri: `https://uploads.mangadex.org/covers/${item.id}/${item.cover_art}.256.jpg`,
+                }}
+              />
+              <Text style={styles.mangaName}>{item.name}</Text>
+            </View>
+          </TouchableOpacity>
+        }
+      />
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
-  container: { width: 100 },
+  container: { width: "100%" },
+  mangaList: {
+    borderWidth: 1,
+    borderColor: "black"
+  },
+  item: {
+    flexDirection: 'row',
+    width: "100%",
+    padding: 5
+  },
+  mangaName: {
+    width: "80%"
+  },
+  thumbnail: {
+    width: "20%",
+    height: 75,
+  }
 });
--- App/Views/Home/index.tsx
+++ App/Views/Home/index.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
 import { Button, SafeAreaView, StyleSheet, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Cards } from '../../components/Cards';
+// import { Cards } from '../../components/Cards';
 import { MangaList } from '../../Component/MangaList';
-import { decrement, increment } from '../../redux/Counter/counterReducer';
+import { MangaDetail } from '../../Component/MangaDetail';
 import { useAppDispatch, useAppSelector } from '../../redux/Hooks';
-
-export const Home: React.FC = () => {
-  const count = useAppSelector((state) => state.persist.counter.value);
+export const Home = () => {
   const dispatch = useAppDispatch();
-  const onIncrease = (): void => {
-    dispatch(increment());
-  };
-  const onDecrease = (): void => {
-    dispatch(decrement());
-  };
   return (
     <SafeAreaView style={styles.container}>
       <MangaList></MangaList>
-      <Text>{count}</Text>
-      <Button onPress={onIncrease} title="+" />
-      <Button onPress={onDecrease} title="-" />
-      <Icon size={20} name="glass" />
+      <MangaDetail></MangaDetail>
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
   container: {},
+  mangaList: { width: 100 },
 });
