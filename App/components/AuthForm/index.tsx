8cb59960f938a294640c160f1e17eedcc93e0935
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2021-07-06 00:10:14 +0300

    clean up code

--- App/components/AuthForm/index.tsx
+++ App/components/AuthForm/index.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import {
-  FlatList,
-  Button,
-  SafeAreaView,
-  View,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
-import { TextInput } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { useAppDispatch, useAppSelector } from '../../redux/Hooks';
+import { useAppSelector } from '../../redux/Hooks';
 import { LoginForm } from './LoginForm';
 import { LogoutForm } from './LogoutForm';
 
-export const AuthForm = () => {
+export const AuthForm = (): Element => {
   const user = useAppSelector((state) => state.persist.user);
-  const dispatch = useAppDispatch();
   if (!user.loggedIn) return <LoginForm />;
   else return <LogoutForm />;
 };
-const styles = StyleSheet.create({});
