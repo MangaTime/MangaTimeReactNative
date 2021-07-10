d217ee99ab7cb030e6736572cc9d4b7a0f5cc360
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-10 21:17:27 +0300

    feat: update tab styling

--- App/Navigator/index.tsx
+++ App/Navigator/index.tsx
@@ -1,6 +1,7 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { CombinedDefaultThemeNavigation } from '../Theme';
 import { Home } from '../Views/Home';
 import { Settings } from '../Views/Settings';
@@ -10,9 +11,34 @@ const Tab = createMaterialBottomTabNavigator();
 export const Navigator = () => {
   return (
     <NavigationContainer theme={CombinedDefaultThemeNavigation}>
-      <Tab.Navigator>
-        <Tab.Screen name={AppViews.HOME} component={Home} />
-        <Tab.Screen name={AppViews.SETTINGS} component={Settings} />
+      <Tab.Navigator shifting>
+        <Tab.Screen
+          name={AppViews.HOME}
+          component={Home}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Icon name="home" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={AppViews.BROWSE}
+          component={Settings}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Icon name="library-books" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={AppViews.SETTINGS}
+          component={Settings}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Icon name="settings" color={color} size={24} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
