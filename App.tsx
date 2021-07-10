21f2463c71a445784b29df6fa3b5506ff42e0eb0
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-10 20:49:03 +0300

    feat: Apply theme to navigator and app

--- App.tsx
+++ App.tsx
@@ -1,51 +1,17 @@
 import React from 'react';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Navigator } from './App/Navigator';
 import { persistor, store } from './App/redux/store';
+import { CombinedDefaultTheme } from './App/Theme';
 
-// Default theme obj
-// const DefaultTheme: Theme = {
-//   dark: false,
-//   roundness: 4,
-//   colors: {
-//     primary: '#6200ee', //Color
-//     accent: '#03dac4',  //Dark
-//     background: '#f6f6f6', //Light
-//     surface: white,
-//     error: '#B00020',
-//     text: black,
-//     onSurface: '#ffffff',
-//     disabled: color(black).alpha(0.26).rgb().string(),
-//     placeholder: color(black).alpha(0.54).rgb().string(),
-//     backdrop: color(black).alpha(0.5).rgb().string(),
-//     notification: pinkA400,
-//   },
-//   fonts: configureFonts(),
-//   animation: {
-//     scale: 1.0,
-//   },
-// };
-
-const theme = {
-  ...DefaultTheme,
-  dark: true,
-  roundness: 5,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#212121',
-    accent: '#000000',
-    background: '#484848',
-    text: '#ffffff',
-  },
-};
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <PaperProvider theme={theme}>
+        <PaperProvider theme={CombinedDefaultTheme}>
           <SafeAreaProvider>
             <Navigator />
           </SafeAreaProvider>
--- App/Navigator/index.tsx
+++ App/Navigator/index.tsx
@@ -1,13 +1,15 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { CombinedDefaultThemeNavigation } from '../Theme';
 import { Home } from '../Views/Home';
 import { Settings } from '../Views/Settings';
 import AppViews from './AppViews';
+
 const Tab = createMaterialBottomTabNavigator();
 export const Navigator = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={CombinedDefaultThemeNavigation}>
       <Tab.Navigator>
         <Tab.Screen name={AppViews.HOME} component={Home} />
         <Tab.Screen name={AppViews.SETTINGS} component={Settings} />
