b10b3940390e337beaf2fb6b2fa3417f463c26ac
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-10 16:55:50 +0300

    feat: update setting

--- App/Views/Settings/index.tsx
+++ App/Views/Settings/index.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import { Appbar, Avatar, Button, useTheme } from 'react-native-paper';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const Settings = () => {
+  const { colors, dark } = useTheme();
+  const insets = useSafeAreaInsets();
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Setting</Text>
-    </SafeAreaView>
+    <>
+      <View
+        style={[
+          styles.statusBarColor,
+          {
+            backgroundColor: colors.accent,
+            height: insets.top,
+          },
+        ]}
+      />
+      <StatusBar
+        backgroundColor="transparent"
+        barStyle={!dark ? 'dark-content' : 'light-content'}
+        translucent
+      />
+      <Appbar>
+        <Avatar.Icon size={50} icon="account-circle" />
+        <Appbar.Content title="Setting" />
+        <Button
+          mode="contained"
+          style={{ ...styles.button, ...{ backgroundColor: colors.accent } }}
+          onPress={() => console.log('test')}>
+          Login
+        </Button>
+      </Appbar>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { width: 100 },
+  statusBarColor: { width: '100%' },
+  button: { borderRadius: 20 },
 });
