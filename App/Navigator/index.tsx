41955409611519e70abd8acbcf5e1f392c7ef84d
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-10 20:18:20 +0300

    feat: lib + use materialTabs

--- App/Navigator/index.tsx
+++ App/Navigator/index.tsx
@@ -1,11 +1,10 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { Home } from '../Views/Home';
 import { Settings } from '../Views/Settings';
 import AppViews from './AppViews';
-
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 export const Navigator = () => {
   return (
     <NavigationContainer>
