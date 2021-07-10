ca25f8676a31e215b7cacdd1c1b961212c54c42e
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-10 21:34:09 +0300

    feat: added white theme

--- App/Theme/index.tsx
+++ App/Theme/index.tsx
@@ -1,7 +1,7 @@
 import { DefaultTheme as NavigationDefaultTheme } from '@react-navigation/native';
 import { DefaultTheme as PaperDefaultTheme } from 'react-native-paper';
 
-export const CombinedDefaultTheme = {
+const Black = {
   ...PaperDefaultTheme,
   ...NavigationDefaultTheme,
   dark: true,
@@ -9,12 +9,32 @@ export const CombinedDefaultTheme = {
     ...PaperDefaultTheme.colors,
     ...NavigationDefaultTheme.colors,
     ...PaperDefaultTheme.colors,
-    primary: '#212121',
-    accent: '#000000',
-    background: '#484848',
+    primary: '#212121', // Normal variant
+    accent: '#000000', // Dark variant
+    background: '#484848', // Light variant
     text: '#ffffff',
-    card: '#212121',
+    card: '#212121', // Normal variant
   },
+};
+
+const White = {
+  ...PaperDefaultTheme,
+  ...NavigationDefaultTheme,
+  dark: true,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    ...NavigationDefaultTheme.colors,
+    ...PaperDefaultTheme.colors,
+    primary: '#BDBDBD',
+    accent: '#8D8D8D',
+    background: '#EFEFEF',
+    text: '#000000',
+    card: '#BDBDBD',
+  },
+};
+
+export const CombinedDefaultTheme = {
+  ...Black,
 };
 
 // Because navigation is a bitch to theme
