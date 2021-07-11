e4594924d2e08ee3beb17408a51b51a1fe256244
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-11 15:09:19 +0300

    feat: added baseColors

---
+++ App/Theme/baseColors.ts
@@ -0,0 +1,12 @@
+export const baseColors = {
+  black: {
+    normal: '#212121',
+    light: '#484848',
+    dark: '#000000',
+  },
+  white: {
+    normal: '#BDBDBD',
+    light: '#EFEFEF',
+    dark: '#8D8D8D',
+  },
+};
