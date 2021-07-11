c0f6fe721fcae9c7dc0395062eb2a337b4f0ca96
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-11 15:07:21 +0300

    feat: update theme reducer

--- App/redux/Theme/themeReducer.ts
+++ App/redux/Theme/themeReducer.ts
@@ -1,18 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { AppTheme } from '../../Theme';
+import { AppTheme, ThemeName } from '../../Theme';
 
 export interface ThemeState {
   theme: any;
+  themeName: string;
 }
 const initialState: ThemeState = {
   theme: AppTheme.White,
+  themeName: ThemeName.White,
 };
 export const themeSlice = createSlice({
   name: 'theme',
   initialState,
   reducers: {
     changeTheme: (state: ThemeState, action: PayloadAction<any>) => {
-      return { ...state, theme: action.payload };
+      return {
+        ...state,
+        theme: action.payload.theme,
+        themeName: action.payload.themeName,
+      };
     },
   },
 });
