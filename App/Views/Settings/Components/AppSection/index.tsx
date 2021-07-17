10b500077853d045ec00eec4682e27d0b7c3e50a
hoangtran-97 <hoangtranxamk@gmail.com>
2021-07-17 16:00:55 +0300

    Added AppSection component

---
+++ App/Views/Settings/Components/AppSection/index.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SectionTitle } from '../SectionTitle';
+import { ThemeSelector } from '../ThemeSelector';
+
+export const AppSection = (): ReactElement => {
+  return (
+    <View style={styles.container}>
+      <SectionTitle title="App" />
+      <ThemeSelector />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: { margin: 16 },
+});
